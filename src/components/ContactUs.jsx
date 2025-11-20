2b5c0d42d16901669ce7bffe63a5c51d11d727e0
Fajer Ibrahim <fajeribrahim64@gmail.com>
2025-11-20 11:32:45 +0700

    feat: add animation

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast';
+import { motion } from 'motion/react'
 
 const ContactUs = () => {
     const [result, setResult] = React.useState("");
-    
+
     const onSubmit = async (event) => {
         event.preventDefault();
         setResult("Sending....");
@@ -33,9 +34,19 @@ const ContactUs = () => {
     };
 
     return (
-        <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ staggerChildren: 0.2 }}
+            id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
             <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
-            <form className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full' onSubmit={onSubmit}>
+            <motion.form
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+                viewport={{ once: true }}
+                className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full' onSubmit={onSubmit}>
                 <div>
                     <p className='text-sm mb-2 font-medium'>Your Name</p>
                     <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
@@ -57,8 +68,8 @@ const ContactUs = () => {
                 <button type='submit' className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-105 transition-all'>
                     Submit <img src={assets.arrow_icon} alt="" className='w-4' />
                 </button>
-            </form>
-        </div>
+            </motion.form>
+        </motion.div>
     )
 }
 
